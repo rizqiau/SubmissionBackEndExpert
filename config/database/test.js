1f70f42b4c318d88682a4993680c6a349c73330f
rizqiau <mochammadrizqiaullia@gmail.com>
2025-06-22 20:05:46 +0700

    update ci

---
+++ config/database/test.js
@@ -0,0 +1,7 @@
+module.exports = {
+  user: process.env.PGUSER_TEST,
+  password: process.env.PGPASSWORD_TEST,
+  host: process.env.PGHOST_TEST,
+  port: process.env.PGPORT_TEST,
+  database: process.env.PGDATABASE_TEST,
+};
